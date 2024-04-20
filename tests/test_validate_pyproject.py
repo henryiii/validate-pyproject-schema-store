353a7a8a74a6c3aa552c21e8017b9e5629a7981e
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-04-20 12:42:08 -0400

    tests: add tests for hatch env (#33)
    * tests: add tests for hatch env
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * style: pre-commit fixes
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_validate_pyproject.py
+++ tests/test_validate_pyproject.py
@@ -1,19 +1,40 @@
 from __future__ import annotations
 
+import sys
+
 from validate_pyproject import api
+
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
 
 
 def test_vpp_loads(caplog):
-    pyproject_as_dict = {
-        "tool": {
-            "ruff": {
-                "src": ["src"],
-            }
-        }
-    }
+    pyproject_as_dict = tomllib.loads("""
+        [tool.ruff]
+        src = ["src"]
+    """)
+
     validator = api.Validator()
     validator(pyproject_as_dict)
     assert (
         "validate_pyproject_schema_store.schema.get_schema defines `tool.poetry` schema"
+        in caplog.text
+    )
+
+
+def test__loads(caplog):
+    pyproject_as_dict = tomllib.loads("""
+        [tool.hatch.env]
+        requires = [
+            "hatch-pip-compile"
+        ]
+    """)
+
+    validator = api.Validator()
+    validator(pyproject_as_dict)
+    assert (
+        "validate_pyproject_schema_store.schema.get_schema defines `tool.hatch` schema"
         in caplog.text
     )
