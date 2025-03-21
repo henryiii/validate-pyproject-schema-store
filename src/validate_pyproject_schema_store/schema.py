920acf4b318c92204c66babc3b5d21c44b9e5783
Henry Schreiner <HenrySchreinerIII@gmail.com>
2025-03-21 17:55:33 -0400

    feat: support extra schemas with validate-pyproject 0.24 (#117)
    * feat: support extra schemas with validate-pyproject 0.24
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * fix: support priority with 0.24.1
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/validate_pyproject_schema_store/schema.py
+++ src/validate_pyproject_schema_store/schema.py
@@ -30,16 +30,26 @@ def get_tools() -> dict[str, str]:
         return json.load(f)  # type: ignore[no-any-return]
 
 
+@functools.lru_cache
+def get_extra() -> dict[str, str]:
+    extra_file = RESOURCES / "extra.json"
+    with extra_file.open(encoding="utf-8") as f:
+        return json.load(f)  # type: ignore[no-any-return]
+
+
+def _load_schema(filename: str) -> dict[str, Any]:
+    tool_file = RESOURCES / filename
+    with tool_file.open(encoding="utf-8") as f:
+        return json.load(f)  # type: ignore[no-any-return]
+
+
 def get_schema(tool: str) -> dict[str, Any]:
     tools = get_tools()
     if tool not in tools:
         msg = f"Must be valid tool, got {tool}"
         raise AssertionError(msg)
 
-    tool_file = RESOURCES / f"{tool}.schema.json"
-    with tool_file.open(encoding="utf-8") as f:
-        tool_json = json.load(f)
-
+    tool_json = _load_schema(f"{tool}.schema.json")
     orig_tool_json = tool_json.copy()
 
     _, _, path = tools[tool].partition("#")
@@ -50,4 +60,26 @@ def get_schema(tool: str) -> dict[str, Any]:
     tool_json["$schema"] = orig_tool_json["$schema"]
     if "definitions" in orig_tool_json:
         tool_json["definitions"] = orig_tool_json["definitions"]
-    return tool_json  # type: ignore[no-any-return]
+
+    # Nested schemas are not supported with this entry point, only with the multi entry point (validate-pyproject 0.24+)
+    properties = tool_json.get("properties", {})
+    for prop in properties.values():
+        if prop.get("$ref", "").startswith("https://json.schemastore.org/partial"):
+            prop.clear()
+            prop["type"] = "object"
+
+    return tool_json
+
+
+get_schema.priority = -2  # type: ignore[attr-defined]
+
+
+def get_multi_schema() -> dict[str, Any]:
+    tools = get_tools()
+    extras = get_extra()
+
+    return {
+        "tools": {tool: _load_schema(f"{tool}.schema.json") for tool in tools},
+        "schemas": [_load_schema(f"{extra}.schema.json") for extra in extras],
+        "priority": -1,
+    }
--- tests/test_package.py
+++ tests/test_package.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest
 
-from validate_pyproject_schema_store.schema import get_schema
+from validate_pyproject_schema_store.schema import get_multi_schema, get_schema
 
 
 def test_get_unknown_schema():
@@ -26,3 +26,32 @@ def test_get_schema_full(tool: str):
     assert schema["$id"] == f"https://json.schemastore.org/{tool}.json"
     assert schema["$schema"] == "http://json-schema.org/draft-07/schema#"
     assert "properties" in schema
+
+
+def test_no_nested_schema():
+    schema = get_schema("pdm")
+    properties = schema["properties"]
+    for prop in properties.values():
+        assert not prop.get("$ref", "")
+
+
+def test_multi_schema():
+    multi_schema = get_multi_schema()
+    assert (
+        multi_schema["tools"]["ruff"]["$id"] == "https://json.schemastore.org/ruff.json"
+    )
+    assert (
+        multi_schema["tools"]["ruff"]["$schema"]
+        == "http://json-schema.org/draft-07/schema#"
+    )
+
+    assert (
+        multi_schema["tools"]["pdm"]["properties"]["dockerize"]["$ref"]
+        == "https://json.schemastore.org/partial-pdm-dockerize.json"
+    )
+    (pdm_docker,) = [
+        s
+        for s in multi_schema["schemas"]
+        if s["$id"] == "https://json.schemastore.org/partial-pdm-dockerize.json"
+    ]
+    assert pdm_docker["$schema"] == "http://json-schema.org/draft-07/schema#"
